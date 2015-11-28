ee752a1ad17ee92b4eed27f02039004be66c6635
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-11-28 14:31:19 +0000

    Add entity subsystem

---
+++ deps/ecs/src/main/java/me/jackwilsdon/ecs/core/EntitySubSystem.java
@@ -0,0 +1,44 @@
+package me.jackwilsdon.ecs.core;
+
+import me.jackwilsdon.ecs.util.filter.AcceptAllFilter;
+import me.jackwilsdon.ecs.util.filter.Filter;
+
+import java.util.Set;
+
+public abstract class EntitySubSystem extends SubSystem {
+    private Filter<Entity> entityFilter;
+    private Set<Entity> entities;
+
+    public EntitySubSystem(Filter<Entity> entityFilter) {
+        this.entityFilter = entityFilter;
+    }
+
+    public EntitySubSystem() {
+        this(new AcceptAllFilter<Entity>());
+    }
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        super.addedToEngine(engine);
+
+        entities = engine.getEntities(entityFilter);
+    }
+
+    @Override
+    public void removedFromEngine(Engine engine) {
+        super.removedFromEngine(engine);
+
+        entities = null;
+    }
+
+    @Override
+    public void onTick(float deltaTime) {
+        super.onTick(deltaTime);
+
+        for (Entity entity : entities) {
+            processEntity(entity);
+        }
+    }
+
+    public abstract void processEntity(Entity entity);
+}
