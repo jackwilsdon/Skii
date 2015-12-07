cc6081e4674ba1251968c563de29fddce33b4f1a
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-12-07 16:59:37 +0000

    Add subsystem getter to engine

--- deps/ecs/src/main/java/me/jackwilsdon/ecs/core/Engine.java
+++ deps/ecs/src/main/java/me/jackwilsdon/ecs/core/Engine.java
@@ -1,15 +1,18 @@
 package me.jackwilsdon.ecs.core;
 
+import me.jackwilsdon.ecs.util.UniqueClassSetWrapper;
 import me.jackwilsdon.ecs.util.filter.AcceptAllFilter;
 import me.jackwilsdon.ecs.util.filter.Filter;
 import me.jackwilsdon.ecs.util.filter.FilteredSetWrapper;
 import me.jackwilsdon.ecs.util.UniqueEntitySetWrapper;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Engine {
     private UniqueEntitySetWrapper entities = new UniqueEntitySetWrapper(new HashSet<Entity>());
+    private UniqueClassSetWrapper<SubSystem> subSystems = new UniqueClassSetWrapper<>(new HashSet<SubSystem>());
 
     public Set<Entity> getEntities(Filter<Entity> entityFilter) {
         return new FilteredSetWrapper<>(entities, entityFilter);
@@ -25,5 +28,9 @@ public class Engine {
 
     public boolean removeEntity(Entity entity) {
         return entities.remove(entity);
+    }
+
+    public Set<SubSystem> getSubSystems() {
+        return Collections.unmodifiableSet(subSystems);
     }
 }
