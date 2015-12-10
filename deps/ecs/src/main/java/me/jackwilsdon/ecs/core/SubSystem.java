ce7e0a3683600dac43847dae8104f16ae8b1a8c2
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-12-10 00:27:53 +0000

    Add new lines between method definitions

--- deps/ecs/src/main/java/me/jackwilsdon/ecs/core/SubSystem.java
+++ deps/ecs/src/main/java/me/jackwilsdon/ecs/core/SubSystem.java
@@ -2,7 +2,10 @@ package me.jackwilsdon.ecs.core;
 
 public interface SubSystem {
     void onAddedToEngine(Engine engine);
+
     void onRemovedFromEngine(Engine engine);
+
     void onTick(float deltaTime);
+
     void onFrame();
 }
