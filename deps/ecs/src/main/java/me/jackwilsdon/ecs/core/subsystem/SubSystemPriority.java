8514089f2d397894998b9cec9da4db68140c1e95
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-12-13 23:14:52 +0000

    Add subsystem priority class

---
+++ deps/ecs/src/main/java/me/jackwilsdon/ecs/core/subsystem/SubSystemPriority.java
@@ -0,0 +1,13 @@
+package me.jackwilsdon.ecs.core.subsystem;
+
+public final class SubSystemPriority {
+    public static final int NONE = 1000;
+    public static final int LOW = 750;
+    public static final int MEDIUM = 500;
+    public static final int HIGH = 250;
+    public static final int EXTREME = 0;
+
+    private SubSystemPriority() {
+
+    }
+}
